e46ca6249a41e9bcb586012311664619b2dac0e6
Steven Milne <stevenimilne@outlook.com>
2021-04-25 12:47:07 +0100

    Prevent running on non pull request events

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
-import { getInput, setOutput } from '@actions/core'
+import { getInput, setOutput, warning, error } from '@actions/core'
 import { getOctokit, context } from '@actions/github'
+import * as github from './util/github'
 // import Badge from './util/badge'
 
-// const inputs: Badge[] = []
+if (context.eventName !== github.Event.PULL_REQUEST) {
+  error(`Badger does not support ${context.eventName} actions.`)
+} else if (context.action !== github.PullRequestAction.OPENED) {
+  warning(`Skipping Badger, cannot handle ${context.action} events.`)
+} else {
+  const body = context.payload.pull_request.body
+  const badges: string[] = []
 
-for (let i = 1; i <= 10; i++) {
-  const index = i < 10 ? `0${i}` : i
-  const input = getInput(`badge-${index}`)
-
-  if (input) {
-    // inputs.push(Badge.fromString(input))
-    console.log(input)
+  for (let i = 1; i <= 10; i++) {
+    const index = i < 10 ? `0${i}` : i
+    const input = getInput(`badge-${index}`)
+  
+    if (input) {
+      badges.push(input)
+    }
   }
-}
 
-console.log('Context:', JSON.stringify(context))
+  console.log(`Badges: ${JSON.stringify(badges)}`)
+  console.log(`Body:\n${body}`)
+}
